f503fb7366c531b3d981fcc2c7f46270f4831916
yinmin_soe <yinminsoe@gmail.com>
2021-02-12 11:58:20 +0800

    Recipe Implementation

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,38 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+
 @Controller
 public class IndexController {
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 
 	@RequestMapping({"","/", "/index"})
 	public String getIndexPage() {
 		System.out.println("This is Index Controller");
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+		System.out.println("category id "+categoryOptional.get().getId());
+		System.out.println("unit of measure id "+unitOfMeasureOptional.get().getId());
 		return "index";
 	}
+	
+	
+	
 }
